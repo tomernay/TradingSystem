398616a8e76659a0f180acd8a16d0a9ab92081d3
giliooo <gilio@post.bgu.ac.il>
2024-05-13 17:19:07 +0300

    version 1.0 use case 2.3

--- Domain/Store.java
+++ Domain/Store.java
@@ -1,4 +1,43 @@
 package Domain;
 
 public class Store {
+    private int id;
+    private String name;
+    private Inventory inventory;
+
+    // Constructor
+    public Store(int id, String name, Inventory inventory) {
+        this.id = id;
+        this.name = name;
+        this.inventory = inventory;
+    }
+
+    // Getter and setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    // Getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and setter for inventory
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
 }
