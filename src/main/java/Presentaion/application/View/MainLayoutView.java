fee3c59d1e438bac2d54e140cf5e549e93737b1d
Reut Arad <reutara@post.bgu.ac.il>
2024-06-06 13:25:17 +0300

    added buttons for - user, notifications, categories and to the stores

--- src/main/java/Presentaion/application/View/MainLayoutView.java
+++ src/main/java/Presentaion/application/View/MainLayoutView.java
@@ -3,22 +3,23 @@ package Presentaion.application.View;
 import Presentaion.application.Presenter.MainLayoutPresenter;
 import Presentaion.application.View.Messages.MessagesList;
 import Presentaion.application.View.Payment.PaymentPage;
-import Service.UserService;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
+import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Footer;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Header;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -26,14 +27,6 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import com.vaadin.flow.component.textfield.TextField;
-//import com.vaadin.flow.component.applayout.*;
-//import com.vaadin.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations;
-
-import javax.naming.directory.SearchControls;
-import java.awt.*;
 
 
 /**
@@ -48,19 +41,124 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
 
     private H1 viewTitle;
 
+//    private MultiSelectListBox<String> categories;
+
+
 
     public MainLayoutView(MainLayoutPresenter presenter) {
         addClassName("main-view");
         this.presenter = presenter;
         this.presenter.attachView(this);
+        //add a section to be the center of the page
+
         setPrimarySection(Section.DRAWER);
+
         addDrawerContent();
         addHeaderContent();
-        addLogoutButton();
+        myStoresButton();
+//        addLogoutButton();
+        addUserButton();
         welcomeText();
+//        shopByCategory();
         addSearchBar();
+        addNotificationButton();
         shoppingCart();
+        addCategoriesButton();
 
+    }
+
+    private void addUserButton() {
+        // Navigate to the shopping cart page
+        //dropdown menu
+
+        Button userButton = new Button("");
+        ContextMenu dropdownMenu = new ContextMenu(userButton);
+        dropdownMenu.setOpenOnClick(true);
+        //icon
+        userButton.setIcon(new Icon(VaadinIcon.USER));
+        MenuItem personalSettings = dropdownMenu.addItem("Personal Settings", e -> {});
+        MenuItem logout = dropdownMenu.addItem("Logout", e -> {
+            presenter.logout();
+        });
+
+        userButton.getElement().getStyle().set("color", "black");
+        userButton.getElement().getStyle().set("margin-right", "10px"); // Add a margin to the right side of the search button
+        addToNavbar(userButton);
+    }
+
+    private void myStoresButton() {
+        // Navigate to the shopping cart page
+        Button myStores = new Button("My Stores", e -> {
+            // Navigate to the shopping cart page
+        });
+        myStores.getElement().getStyle().setColor("black");
+//        myStores.getElement().getStyle().set("margin-right", "10px"); // Add a margin to the right side of the search button
+        addToDrawer(myStores);
+    }
+
+    private void addCategoriesButton() {
+        //add a button for each category
+
+        //add a button for each category
+        Button category1 = new Button("Food", e -> {
+            // Navigate to the shopping cart page
+        });
+
+        //make the button round
+
+//        category1.getElement().getStyle().set("border-radius", "50px");
+        Button category2 = new Button("Electronics", e -> {
+            // Navigate to the shopping cart page
+        });
+        Button category3 = new Button("Clothing", e -> {
+            // Navigate to the shopping cart page
+        });
+        Image books = new Image("static/books.jpg", "books");
+        books.setWidth("50px");
+        books.setHeight("50px");
+//        addClassName("books-button");
+        Button category4 = new Button(books, e -> {
+
+            // Navigate to the shopping cart page
+        });
+        Button category5 = new Button("Toys", e -> {
+            // Navigate to the shopping cart page
+        });
+
+        category1.addClassName("round-button");
+        category2.addClassName("round-button");
+        category3.addClassName("round-button");
+        category4.addClassName("round-button");
+        category5.addClassName("round-button");
+
+        category1.getElement().getStyle().set("color", "black");
+        category2.getElement().getStyle().set("color", "black");
+        category3.getElement().getStyle().set("color", "black");
+        category4.getElement().getStyle().set("color", "black");
+        category5.getElement().getStyle().set("color", "black");
+
+
+        //the text of the button at the bottom
+
+
+        HorizontalLayout categoriesLayout = new HorizontalLayout(category1, category2, category3, category4, category5);
+        categoriesLayout.addClassName("center-layout");
+        categoriesLayout.setSpacing(true);
+        categoriesLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        categoriesLayout.setWidthFull();
+
+        VerticalLayout mainLayout = new VerticalLayout(categoriesLayout);
+        mainLayout.setSizeFull();
+        mainLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        mainLayout.setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+
+        // Add the main layout to the main content area
+        setContent(mainLayout);
+
+
+//        categoriesLayout.add(category1, category2, category3, category4, category6);
+
+//        addToNavbar(categoriesLayout);
     }
 
     private void addHeaderContent() {
@@ -94,51 +192,37 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
         return nav;
     }
 
-    @Override
-    public void beforeEnter(BeforeEnterEvent event) {
-        // Check if the user is already logged in
-        if (!isLoggedIn()) {
-            // If not logged in, reroute to the login page
-            event.rerouteTo(LoginView.class);
-        }
-    }
 
-    private boolean isLoggedIn() {
-        // Retrieve the current HTTP request
-        HttpServletRequest request = (HttpServletRequest) VaadinService.getCurrentRequest();
 
-        if (request != null) {
-            // Retrieve cookies from the request
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null) {
-                for (Cookie cookie : cookies) {
-                    if ("token".equals(cookie.getName())) {
-                        // Assuming a valid token indicates a logged-in user
-                        return isValidToken(cookie.getValue());
-                    }
-                }
-            }
-        }
+//    private void addCategory1() {
+//        // Navigate to the shopping cart page
+//        Button category1 = new Button("Food", e -> {
+//            // Navigate to the shopping cart page
+//        });
+//
+//
+//    }
+        //add a little down arrow to the right of the button
 
-        // If no valid token is found, the user is not logged in
-        return false;
-    }
+//    private void shopByCategory() {
+//        // Navigate to the shopping cart page
+//        categories = new MultiSelectListBox<>();
+//        categories.setAriaLabel ("Shop by Category");
+//        //add a little down arrow to the right of the button
+////        categories.getElement().getStyle().set("background-image", "url('down-arrow.png')");
+//        //remove background
+//        categories.getElement().getStyle().set("background", "none");
+//        categories.getElement().getStyle().set("border", "none");
+//        Button category = new Button("shop by category", e -> {
+//            // Navigate to the shopping cart page
+//        });
+////        cart.setIcon(new Icon(VaadinIcon.CART));
+//        category.getElement().getStyle().setColor("black");
+//        category.getElement().getStyle().set("margin-right", "10px"); // Add a margin to the right side of the search button
+//        addToNavbar(category);
+//    }
 
-    private boolean isValidToken(String token) {
-        // Implement your token validation logic here
-        // This could involve checking the token against a database or decoding a JWT token
-        return token != null && !token.isEmpty();
-    }
 
-    private void addLogoutButton() {
-        Button logoutButton = new Button("Logout", e -> logout());
-        logoutButton.getElement().getStyle().set("margin-top", "auto"); // This will push the button to the bottom
-        addToDrawer(logoutButton);
-    }
-
-    public void navigateToLogin() {
-        getUI().ifPresent(ui -> ui.navigate("login"));
-    }
 
     public void addSearchBar() {
         // Add search bar to the header
@@ -162,10 +246,19 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
 //        searchBarLayout.addClassName("center-layout"); // Add the CSS class
 //        addToNavbar(searchBarLayout);
 
-
-
         addToNavbar(searchBar, search);
 
+    }
+
+    private void addNotificationButton() {
+        // Add notification button to the header
+        Button notification = new Button("", e -> {
+            // Navigate to the notification page
+        });
+        notification.setIcon(new Icon(VaadinIcon.BELL));
+        notification.getElement().getStyle().setColor("black");
+        notification.getElement().getStyle().set("margin-right", "10px"); // Add a margin to the right side of the search button
+        addToNavbar(notification);
     }
 
     private void shoppingCart() {
@@ -186,15 +279,6 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
 
     }
 
-    private void logout() {
-        presenter.logout();
-    }
-
-    private Footer createFooter() {
-        Footer footer = new Footer();
-        return footer;
-    }
-
     private void welcomeText() {
 //        TextComponent text = new TextComponent();
         Text welcome = new Text("Welcome Guest!");
@@ -204,20 +288,56 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
         welcomeSpan.getStyle().set("white-space", "nowrap");
         //margin between the text and the search bar
         welcomeSpan.getStyle().set("margin-right", "10px");
-//        HorizontalLayout welcomeLayout = new HorizontalLayout(welcomeSpan);
-//        welcomeLayout.addClassName("left-layout"); // Add the CSS class
 
-//        addToNavbar(welcomeLayout);
         addToNavbar(welcomeSpan);
-        //set space between the text and the search bar
-//        welcomeSpan.getElement().getStyle().set("margin-right", "auto");
-//        TextField welcome = new TextField();
-//        welcome ("Welcome Guest!");
-//        welcome.setReadOnly(true);
-        //place the text on the left to the search bar
-//        welcome.getElement().getStyle().set("margin-right", "auto");
 
-//        addToNavbar(welcome);
+    }
+
+    private void logout() {
+        presenter.logout();
+    }
+
+    private Footer createFooter() {
+        Footer footer = new Footer();
+        return footer;
+    }
+
+
+    private boolean isValidToken(String token) {
+        // Implement your token validation logic here
+        // This could involve checking the token against a database or decoding a JWT token
+        return token != null && !token.isEmpty();
+    }
+
+    private void addLogoutButton() {
+        Button logoutButton = new Button("Logout", e -> logout());
+        logoutButton.getElement().getStyle().set("margin-top", "auto"); // This will push the button to the bottom
+        addToDrawer(logoutButton);
+    }
+
+    public void navigateToLogin() {
+        getUI().ifPresent(ui -> ui.navigate("login"));
+    }
+
+    private boolean isLoggedIn() {
+        // Retrieve the current HTTP request
+        HttpServletRequest request = (HttpServletRequest) VaadinService.getCurrentRequest();
+
+        if (request != null) {
+            // Retrieve cookies from the request
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("token".equals(cookie.getName())) {
+                        // Assuming a valid token indicates a logged-in user
+                        return isValidToken(cookie.getValue());
+                    }
+                }
+            }
+        }
+
+        // If no valid token is found, the user is not logged in
+        return false;
     }
 
     @Override
@@ -229,5 +349,14 @@ public class MainLayoutView extends AppLayout implements BeforeEnterObserver {
     private String getCurrentPageTitle() {
         PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
         return title == null ? "" : title.value();
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        // Check if the user is already logged in
+        if (!isLoggedIn()) {
+            // If not logged in, reroute to the login page
+            event.rerouteTo(LoginView.class);
+        }
     }
 }
