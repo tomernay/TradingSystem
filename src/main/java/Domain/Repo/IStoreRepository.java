8fed2beb0d5dd9c309d924e4d8f13ba2c74bee78
GalHalifaBgu <halifag@post.bgu.ac.il>
2024-07-12 19:12:14 +0300

    store aggregative root

---
+++ src/main/java/Domain/Repo/IStoreRepository.java
@@ -0,0 +1,9 @@
+package Domain.Repo;
+
+import Domain.Store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStoreRepository extends JpaRepository<Store, Integer> {
+}
