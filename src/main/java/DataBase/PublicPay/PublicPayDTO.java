61634e1065f740c7a4e1b76a585c41c59c4dff18
Nivn967 <nausn@post.bgu.ac.il>
2024-07-14 20:31:22 +0300

    updated the store repository.

---
+++ src/main/java/DataBase/PublicPay/PublicPayDTO.java
@@ -0,0 +1,78 @@
+//package src.main.java.DataBase.PublicPay;
+//
+//
+//import cn.hutool.json.JSONObject;
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.google.firebase.database.*;
+//import src.main.java.DataBase.Files.FilesHandler;
+//import src.main.java.DataBase.FireBaseConstants;
+//import src.main.java.DataBase.ServiceAccountDetails.Init;
+//import src.main.java.Domain.Store.Inventory.Inventory;
+//import src.main.java.Domain.Store.PurchasePolicy.PaymentTypes.PublicPay;
+//import src.main.java.Domain.Store.Store;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.concurrent.ExecutionException;
+//
+//public class PublicPayDTO {
+//   PublicPay pay;
+//    public PublicPayDTO(PublicPay pay){
+//        this.pay = pay;
+//    }
+//    public static void addPublicPayment(PublicPay payment) throws IOException, ExecutionException, InterruptedException {
+//     try {
+//
+//
+//         Init.initializeFirebaseApp();
+//
+//     }catch (Exception e) {}
+//        JSONObject jsonObject=new JSONObject();
+//      //  jsonObject.put(payment.getStore().getName(), payment).toString();
+//   //   FirebaseDatabase.getInstance().getReference(FireBaseConstants.paymentsRoot).child(FireBaseConstants.publicPay).child(payment.getStore().getName()).setValueAsync(jsonObject).get();
+//
+//    }
+//
+//
+//    public  void retrievePublicPayment(String storeName) throws IOException {
+//
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(FireBaseConstants.paymentsRoot).child(FireBaseConstants.publicPay).child(storeName).child(storeName);
+//
+//        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                try {
+//                    Object o=dataSnapshot.getValue();
+//                     JsonNode json=PublicPayDAO.convertToJsonObject(o);
+//                    FilesHandler.writeJSONObjectToFile(json,new File(FireBaseConstants.publicPay+storeName+".json"));
+//
+//
+//
+//                }
+//                catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.err.println("Error retrieving data: " + databaseError.getMessage());
+//            }
+//        });
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+//        HashMap<String,Integer> products=new HashMap<>();
+//        products.put("1",1);
+//
+//      //addPublicPayment(new PublicPay(1.0,1,new Store("y","y",new Inventory(),"y"),"123",products));
+//
+//     Thread.currentThread().sleep(1000000);
+//    }
+//
+//}
